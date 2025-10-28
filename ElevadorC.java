10c63911663e9badba290968be858c97e4c72eab
ianmendez146 <158490956+ianmendez146@users.noreply.github.com>
2025-10-28 08:50:52 -0600

    Add Elevador2 class with main method

---
+++ ElevadorC.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Elevador2{
+    public static void main(String[] args) {
+        System.out.println("================================");
+        System.out.println("   ");
+        System.out.println("   ") ;
+        System.out.println("      Elevador ");
+        System.out.println("    Piso 10 [ ]");
+        System.out.println("    Piso 9  [ ]");
+        System.out.println("    Piso 8  [ ]");  
+        System.out.println("    Piso 7  [ ]");
+        System.out.println("    Piso 6  [ ]");  
+        System.out.println("    Piso 5  [ ]");
+        System.out.println("    Piso 4  [ ]");
+        System.out.println("    Piso 3  [ ]");
+        System.out.println("    Piso 2  [ ]");
+        System.out.println("    Piso 1  [ ]");
+        System.out.println("================================");
+        System.out.println("    Sotano 1 [ ]");
+        System.out.println("    Sotano 2 [ ]");
+
+
+
+
+
+    }
+}
